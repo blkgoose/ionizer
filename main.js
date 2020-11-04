31d26f29a64b24b8106d5197b52a58fc36f2e5dd
blkgoose <alessio.biancone@gmail.com>
2020-11-04 02:37:23 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  frameRate(60);
+}
+
+accelerating=false
+mouseStartingPositionY=0
+prevMouseY=0
+prevActivation=0
+
+function draw() {
+  fetch("http://localhost:3000/info/1f9c75b0-6edc-36a4-a15b-279b8b531ca8")
+    .then(r => r.json())
+    .then(r => {
+      prevActivation=r.thruster.activation
+
+      clear()
+      ambientLight(255, 255, 255)
+      translate(r.body.position[0], r.body.position[1], r.body.position[2])
+      push()
+      ambientMaterial(255, 0, 0);
+      sphere(50)
+      pop()
+    })
+
+  if(keyIsPressed && key=='a' && !accelerating) {
+    accelerating=true
+    mouseStartingPositionY=mouseY
+    prevMouseY=mouseY
+  }
+  if(!keyIsPressed){
+    accelerating=false
+  }
+  if(accelerating && prevMouseY!=mouseY) {
+    console.log("activation: ", JSON.stringify({activation: prevActivation+((mouseStartingPositionY-mouseY)*.1)}))
+
+    fetch("http://localhost:3000/command/1f9c75b0-6edc-36a4-a15b-279b8b531ca8/thruster",
+      { method: "POST"
+      , headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+      , body: JSON.stringify({activation: prevActivation+((mouseStartingPositionY-mouseY)*.1)})
+      }
+    )
+
+    prevMouseY=mouseY
+  }
+}
+
